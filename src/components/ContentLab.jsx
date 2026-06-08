6dd15dad833bb6414f8c60b7e7693f2265505baa
alecadario <ale@alecadario.com>
2026-06-08 19:09:35 +0200

    Add ContentLab component

---
+++ src/components/ContentLab.jsx
@@ -0,0 +1,22 @@
+/* global React */
+const { useState, useEffect, useCallback, useMemo, useRef } = React;
+
+import {
+  AirtableAPI, TABLE_IDS, CURRENT_USER, COMPANY_PROFILE,
+  callOpenAI, navSetUrl, SOURCE_OPTIONS, OPP_STAGES, WON_STAGES, CLOSED_STAGES,
+  BENCH_REPLY_HIGH, BENCH_REPLY_LOW, BENCH_MEETING_HIGH, BENCH_MEETING_LOW,
+  CHANNEL_BENCHMARKS, MESSAGE_PROMPTS, MESSAGE_PROMPT_DEFAULTS, saveMessagePrompts,
+  resolvePromptTemplate, saveCompanyProfile, channelIcon, logoutUser,
+  CLIENT_CONFIG, AUTH_TOKEN, loadBranding, BRANDING_LS_KEY,
+} from '../globals.js';
+import {
+  F, linkedIds, resolveLinked, InfoTip, REPLY_STATUSES, computeEnrichment,
+  DIAGNOSIS_CONFIG, findDuplicateStakeholder, confirmDuplicateStakeholder,
+  deriveStakeholderStatus, updateStakeholderStatus, STAKEHOLDER_STATUS_PRIORITY,
+  STAKEHOLDER_STATUS_PROTECTED, activateAccountIfNeeded,
+  formatCurrency, formatDate, strSimilarity, FileNotesRenderer,
+} from '../utils.js';
+
+PLACEHOLDER_CONTENT
+
+export default ContentLab;
